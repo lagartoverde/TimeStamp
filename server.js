514c6ff2b5e22243d87993baeacae3d5859d248d
Oscar Rodriguez Villalobos <lagartoverde97@gmail.com>
2016-12-15 11:06:13 +0100

    Finished the system requirements of the API

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var http=require("http");
+var url=require("url");
 
 var server=http.createServer(function(req,res){
-	var d=new Date("3 June 2017");
-	res.write(d.getYear());
+	var query=url.parse(req.url).pathname;
+	var queryWithoutSlash = query.substring(1);
+	var queryDecode=decodeURI(queryWithoutSlash);
+	console.log("Request for date "+queryDecode);
+	if(parseInt(queryDecode)==queryDecode){
+		queryDecode=parseInt(queryDecode);
+	}
+	var date=new Date(queryDecode);
+	var resultObj={
+		"unix":date.getTime(),
+		"natural":date.toDateString()
+	}
+	res.write(JSON.stringify(resultObj));
 	res.end();
 });
 server.listen(process.env.PORT ||8080);
